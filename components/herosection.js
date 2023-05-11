c44fe3b1a341db820c42a1e52a8e225131677a0e
joseph-anselm <iamjoeanselm@gmail.com>
2023-05-11 08:44:22 +0100

    admin fix

--- components/herosection.js
+++ components/herosection.js
@@ -25,7 +25,7 @@ const HeroSection = () => {
           />
         </div>
         <div className="w-1/2">
-          <h1 className="text-white lg:text-5xl md:text-4xl sm:text-2xl font-bold mb-8 pr-3 ">
+          <h1 className="text-white lg:text-5xl md:text-4xl sm:text-2xl font-bold mb-5 pr-3 ">
           Excellence, Creativity, Value.
           </h1>
         </div>
